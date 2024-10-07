aa6b8fd1e907b55d34bf0a24e558337dc922e9c1
ssk2 <1sqlroot@gmail.com>
2024-10-07 11:02:42 +0400

    interfase test2

--- main.go
+++ main.go
@@ -13,7 +13,17 @@ type Email struct {
 }
 
 func (e *Email) Send(msg string) error {
-	fmt.Printf("Message \"%v\" for going email %v \n", msg, e.Addres)
+	fmt.Printf("Message \"%v\" sent to the email %v \n", msg, e.Addres)
+	return nil
+}
+
+type Phone struct {
+	Number  int
+	Balance int
+}
+
+func (p *Phone) Send(msg string) error {
+	fmt.Printf("Message \"%v\" sent to the for number phone %v \n", msg, p.Number)
 	return nil
 }
 
@@ -29,4 +39,7 @@ func Notify(s Sender) {
 func main() {
 	email := &Email{"m82469"}
 	Notify(email)
+
+	phone := &Phone{Number: 7777, Balance: 10}
+	Notify(phone)
 }
